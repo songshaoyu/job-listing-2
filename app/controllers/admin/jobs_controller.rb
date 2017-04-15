9342da0bedc902ebf4613e5832173ee3f36856ef
songshaoyu <ahu8756@gmail.com>
2017-04-15 20:31:47 +0800

    add hide/public button

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,8 @@
+
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :require_is_admin
-
+  layout "admin"
   def index
     @jobs = Job.all
   end
@@ -41,6 +42,20 @@ class Admin::JobsController < ApplicationController
     @job.destroy
     redirect_to admin_jobs_path
   end
+
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
+  end
+
 
   private
 
