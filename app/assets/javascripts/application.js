d2ac8430af36255da2a163ae63cda92fa49ac104
songshaoyu <ahu8756@gmail.com>
2017-04-21 14:52:16 +0800

    删除复制的代码

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,19 +17,4 @@
 //= require bootstrap/dropdown
 //= require bootstrap/modal
 //= require_tree .
-
-
-$(function () {
-  var w = $(window);
-  w.scroll(function () {
-    if (w.scrollTop() !== 0) {
-      $(".d").removeClass("visible");
-      return;
-    }
-
-    $(".d").addClass("visible");
-  });
-
-  $(".d").addClass("visible");
-
-});
+  
